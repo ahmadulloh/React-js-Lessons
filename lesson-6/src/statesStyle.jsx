161a69fc05fc1db43ead90ef60459c845587fc59
ahmadulloh <ahmadillo.web@gmail.com>
2023-03-01 21:25:58 +0500

    styled components

---
+++ lesson-6/src/statesStyle.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled("div")`
+  background-color: #000;
+  padding: 10px 25px;
+`;
+export const HelloWorld = styled('h1')`
+  font-size: 40px;
+  color: #fff;
+`
